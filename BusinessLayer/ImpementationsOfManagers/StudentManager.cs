5619f7410a735f707424dcf867201f1bb9d02931
Kaan Çolak <75448807+kaanxcolak@users.noreply.github.com>
2023-05-15 11:48:42 +0300

    Add files via upload

---
+++ BusinessLayer/ImpementationsOfManagers/StudentManager.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BusinessLayer.InterfacesOfManagers;
+using DataLayer.InterfacesOfRepo;
+using EntityLayer.Models;
+using EntityLayer.ViewModels;
+
+namespace BusinessLayer.ImpementationsOfManagers
+{
+    public class StudentManager : Manager<StudentVM, Student, int>, IStudentManager
+    {
+        public StudentManager(IStudentRepo repo, IMapper mapper) : base(repo, mapper, "Floor")
+        {
+
+        }
+    }
+}
---
+++ BusinessLayer/InterfacesOfManagers/IStudentManager.cs
@@ -0,0 +1,8 @@
+﻿using EntityLayer.ViewModels;
+
+namespace BusinessLayer.InterfacesOfManagers
+{
+    public interface IStudentManager : IManager<StudentVM,int>
+    {
+    }
+}
